a6f8b7877b8fbeed24ac8c290d787256aa9880c5
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-13 15:29:02 -0300

    Adding elements to the home template.

--- settings/settings.py
+++ settings/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_FILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'templates/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
