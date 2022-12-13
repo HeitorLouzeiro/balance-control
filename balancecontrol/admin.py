59fd8a8180fa236ce861986464d5a391e66314cd
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-13 15:45:34 -0300

    Added models in the project.

--- balancecontrol/admin.py
+++ balancecontrol/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Balance
+
+
 # Register your models here.
+class BalanceAdmin(admin.ModelAdmin):
+    list_display = ('value', 'typeoperation', 'datecreate')
+    list_filter = ('typeoperation', 'datecreate')
+    list_editable = ('typeoperation',)
+    search_fields = ('typeoperation', 'datecreate')
+    list_per_page = 10
+
+
+admin.site.register(Balance, BalanceAdmin)
--- balancecontrol/models.py
+++ balancecontrol/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Balance(models.Model):
+    OPTION = (
+        ("E", "Entrada"),
+        ("S", "Saída")
+    )
+    value = models.FloatField()
+    typeoperation = models.CharField(max_length=1, choices=OPTION)
+    datecreate = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.value:.2f}'.replace('.', ',')
+
+    def get_value_format(self):
+        return f'{self.value:.2f}'.replace('.', ',')
+
+    class Meta:
+        verbose_name = 'Balance'
+        verbose_name_plural = 'Balances'
+        ordering = ['datecreate']
