eb5efaacd2833adbce939b0049d8205d7a3d78eb
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-09-10 11:13:14 -0300

    Changing database.

--- settings/settings.py
+++ settings/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
+        'PORT': os.environ.get("POSTGRES_PORT"),
+
     }
 }
 
